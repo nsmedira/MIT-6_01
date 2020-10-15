6264ed4037a85dcdb4ae1ab8c143b1993bae0e33
Nick Smedira <nsmedira@me.com>
2020-10-15 13:58:00 -0400

    chapter 2: variables, expressions and statements

---
+++ python-tutorial/types-values-expressions-variables-binding/chapter-2-variables-expressions-statements/type.py
@@ -0,0 +1,8 @@
+# string
+print(type('Hello World'))
+
+# integer
+print(type(4))
+
+# floating point
+print(type(4.5))
---
+++ python-tutorial/types-values-expressions-variables-binding/chapter-2-variables-expressions-statements/variables.py
@@ -0,0 +1,8 @@
+# assignment statements
+message = 'And now for something completely different'
+n = 17
+pi = 3.1415926535897932
+
+print(message)
+print (n)
+print(pi)
