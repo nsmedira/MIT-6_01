5d40b1534479d429cac17f1b78759d397d6a0073
Nick Smedira <nsmedira@me.com>
2020-10-29 19:12:08 -0400

    add wikipedia link

--- python-tutorial/loops_list-comprehensions/chapter-11_dictionaries/exercise7.py
+++ python-tutorial/loops_list-comprehensions/chapter-11_dictionaries/exercise7.py
@@ -5,7 +5,8 @@
 
 # Exponentiation of large integers is the basis of common algorithms for public-key encryption. 
 
-# Read the Wikipedia page on the RSA algorithm3 and write functions to encode and decode messages.
+# Read the Wikipedia page on the RSA algorithm and write functions to encode and decode messages.
+# https://en.wikipedia.org/wiki/RSA_(cryptosystem)#Key_generation
 
 import math
 
