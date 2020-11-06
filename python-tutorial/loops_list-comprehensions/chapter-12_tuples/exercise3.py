5793275e370211195c29302fc742001ede7beadc
Nick Smedira <nsmedira@me.com>
2020-11-06 09:59:24 -0500

    compare result to wikipedia article

--- python-tutorial/loops_list-comprehensions/chapter-12_tuples/exercise3.py
+++ python-tutorial/loops_list-comprehensions/chapter-12_tuples/exercise3.py
@@ -4,7 +4,8 @@
 # Exercise 3
 
 # Write a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. 
-# Find text samples from several different languages and see how letter frequency varies between languages. Compare your results with the tables at wikipedia.org/wiki/Letter_frequencies.
+# Find text samples from several different languages and see how letter frequency varies between languages. 
+# Compare your results with the tables at wikipedia.org/wiki/Letter_frequencies.
 
 def most_frequent(s):
 
@@ -38,4 +39,4 @@ if __name__ == "__main__":
     # italian sample
     italian = "Negli sguardi del popolo della Rossalleggi la fierezza di appartenere a una leggenda. Entri nel cuore della Ferrari e vedi l’ultimo modello della ‘ Roma’ che sta salendo sulla rampa di un camion (\"parte per la consegna a un cliente\"), mentre a destra si erge il ‘Centro sviluppo prodotto’ progettato da Fuksas (\"nel 2019 sono state realizzate non più di 10.131 auto\", l’esclusività, il rimanere sotto gli ordinativi effettivi è parte fondamentale del valore Ferrari). Quindi è la volta delle linee di montaggio dove prendono vita 35-40 vetture al giorno e, subito dopo, della fucina delle idee commissionata da Sergio Marchionne per inserire in un unico edificio quattro componenti strategiche: Ferrari Design (che include un’ottantina di progettisti e designers delle auto), la Modelleria per la predisposizione delle scocche, il Taylor Made e l’Atelier per la personalizzazione dei modelli."
 
-    most_frequent(italian)
+    most_frequent(english)
